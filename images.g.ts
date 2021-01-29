2b9f1f62b18cb8ccc0aac891d58a2aa49bd2abf8
erictan88 <69663089+erictan88@users.noreply.github.com>
2021-01-29 21:04:52 +0800

    Update main.blocks, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,25 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . 6 6 6 6 6 6 6 6 . . 
-. . . . . 6 c 6 6 6 6 6 6 9 6 . 
-. . . . 6 c c 6 6 6 6 6 6 9 c 6 
-. . d 6 9 c c 6 9 9 9 9 9 9 c c 
-. d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
-. 6 6 6 9 b 8 8 b b b 8 b b 8 6 
-. 6 6 6 6 6 8 b b b b 8 b b b 8 
-. 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
-. 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
-. d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
-. 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
-. 8 8 8 8 f f f 8 8 8 8 f f f f 
-. . . 8 f f f f f 8 8 f f f f f 
-. . . . f f f f . . . . f f f . 
-. . . . . . . . . . . . . . . . 
+            case "image6":return img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
 `;
-            case "image4":return img`
+            case "image1":return img`
 eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
 eeebbddddddbbeeeeeebbddddddbbeee
 eebbddbbbbddbbeeeebbddbbbbddbbee
@@ -43,61 +43,7 @@ eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 `;
-            case "image5":return img`
-. . . . f f f f f . . . . . . . 
-. . . f e e e e e f . . . . . . 
-. . f d d d d e e e f . . . . . 
-. c d f d d f d e e f f . . . . 
-. c d f d d f d e e d d f . . . 
-c d e e d d d d e e b d c . . . 
-c d d d d c d d e e b d c . . . 
-c c c c c d d e e e f c . . . . 
-. f d d d d e e e f f . . . . . 
-. . f f f f f e e e e f . . . . 
-. . . . f f e e e e e e f . f f 
-. . . f e e f e e f e e f . e f 
-. . f e e f e e f e e e f . e f 
-. f b d f d b f b b f e f f e f 
-. f d d f d d f d d b e f f f f 
-. . f f f f f f f f f f f f f . 
-`;
-            case "image6":return img`
-. . . . c c c b b b b b . . . . 
-. . c c b 4 4 4 4 4 4 b b b . . 
-. c c 4 4 4 4 4 5 4 4 4 4 b c . 
-. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-. e b 4 4 4 4 4 5 4 4 4 4 b e . 
-8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-8 7 2 e e e e e e e e e e 2 7 8 
-e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-e b e 8 8 c c 8 8 c c c 8 e b e 
-e e b e c c e e e e e c e b e e 
-. e e b b 4 4 4 4 4 4 4 4 e e . 
-. . . c c c c c e e e e e . . . 
-`;
-            case "image7":return img`
-. . . . . . . . . . . . . . . . 
-. . . . 3 3 3 3 3 3 3 3 . . . . 
-. . . 3 d 3 3 3 3 3 3 c 3 . . . 
-. . 3 c d 3 3 3 3 3 3 c c 3 . . 
-. 3 c c d d d d d d 3 c c d 3 d 
-. 3 c 3 a a a a a a a b c d 3 3 
-. 3 3 a b b a b b b a a b d 3 3 
-. 3 a b b b a b b b b a 3 3 3 3 
-. a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
-. a a a a a a f a a a f a 3 d d 
-. a a a a a a f a a f a a a 3 d 
-. a a a a a a f f f a a a a a a 
-. a f f f f a a a a f f f a a a 
-. . f f f f f a a f f f f f a . 
-. . . f f f . . . . f f f f . . 
-. . . . . . . . . . . . . . . . 
-`;
-            case "image8":return img`
+            case "image2":return img`
 . . . . . . . . . . . . . . . . 
 . . . . 3 3 3 3 3 3 3 3 . . . . 
 . . . 3 d 3 3 3 3 3 3 c 3 . . . 
@@ -137,7 +83,69 @@ eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 `;
-            case "image2":return img`
+            case "image4":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image5":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image7":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 3 3 3 3 3 3 3 3 . . . . 
+. . . 3 d 3 3 3 3 3 3 c 3 . . . 
+. . 3 c d 3 3 3 3 3 3 c c 3 . . 
+. 3 c c d d d d d d 3 c c d 3 d 
+. 3 c 3 a a a a a a a b c d 3 3 
+. 3 3 a b b a b b b a a b d 3 3 
+. 3 a b b b a b b b b a 3 3 3 3 
+. a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+. a a a a a a f a a a f a 3 d d 
+. a a a a a a f a a f a a a 3 d 
+. a a a a a a f f f a a a a a a 
+. a f f f f a a a a f f f a a a 
+. . f f f f f a a f f f f f a . 
+. . . f f f . . . . f f f f . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image8":return img`
 eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
 eeebbddddddbbeeeeeebbddddddbbeee
 eebbddbbbbddbbeeeebbddbbbbddbbee
@@ -161,20 +169,20 @@ eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 `;
             case "image9":return img`
 . . . . . . . . . . . . . . . . 
-. . . . . . 6 6 6 6 6 6 6 6 . . 
-. . . . . 6 c 6 6 6 6 6 6 9 6 . 
-. . . . 6 c c 6 6 6 6 6 6 9 c 6 
-. . d 6 9 c c 6 9 9 9 9 9 9 c c 
-. d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
-. 6 6 6 9 b 8 8 b b b 8 b b 8 6 
-. 6 6 6 6 6 8 b b b b 8 b b b 8 
-. 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
-. 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
-. d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
-. 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
-. 8 8 8 8 f f f 8 8 8 8 f f f f 
-. . . 8 f f f f f 8 8 f f f f f 
-. . . . f f f f . . . . f f f . 
+. . . . 3 3 3 3 3 3 3 3 . . . . 
+. . . 3 d 3 3 3 3 3 3 c 3 . . . 
+. . 3 c d 3 3 3 3 3 3 c c 3 . . 
+. 3 c c d d d d d d 3 c c d 3 d 
+. 3 c 3 a a a a a a a b c d 3 3 
+. 3 3 a b b a b b b a a b d 3 3 
+. 3 a b b b a b b b b a 3 3 3 3 
+. a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+. a a a a a a f a a a f a 3 d d 
+. a a a a a a f a a f a a a 3 d 
+. a a a a a a f f f a a a a a a 
+. a f f f f a a a a f f f a a a 
+. . f f f f f a a f f f f f a . 
+. . . f f f . . . . f f f f . . 
 . . . . . . . . . . . . . . . . 
 `;
             case "image10":return img`
@@ -244,140 +252,6 @@ eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 `;
             case "image13":return img`
-. . . . f f f f f . . . . . . . 
-. . . f e e e e e f . . . . . . 
-. . f d d d d e e e f . . . . . 
-. c d f d d f d e e f f . . . . 
-. c d f d d f d e e d d f . . . 
-c d e e d d d d e e b d c . . . 
-c d d d d c d d e e b d c . . . 
-c c c c c d d e e e f c . . . . 
-. f d d d d e e e f f . . . . . 
-. . f f f f f e e e e f . . . . 
-. . . . f f e e e e e e f . f f 
-. . . f e e f e e f e e f . e f 
-. . f e e f e e f e e e f . e f 
-. f b d f d b f b b f e f f e f 
-. f d d f d d f d d b e f f f f 
-. . f f f f f f f f f f f f f . 
-`;
-            case "image14":return img`
-. . . . c c c b b b b b . . . . 
-. . c c b 4 4 4 4 4 4 b b b . . 
-. c c 4 4 4 4 4 5 4 4 4 4 b c . 
-. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-. e b 4 4 4 4 4 5 4 4 4 4 b e . 
-8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-8 7 2 e e e e e e e e e e 2 7 8 
-e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-e b e 8 8 c c 8 8 c c c 8 e b e 
-e e b e c c e e e e e c e b e e 
-. e e b b 4 4 4 4 4 4 4 4 e e . 
-. . . c c c c c e e e e e . . . 
-`;
-            case "image15":return img`
-. . . . f f f f f . . . . . . . 
-. . . f e e e e e f . . . . . . 
-. . f d d d d e e e f . . . . . 
-. c d f d d f d e e f f . . . . 
-. c d f d d f d e e d d f . . . 
-c d e e d d d d e e b d c . . . 
-c d d d d c d d e e b d c . . . 
-c c c c c d d e e e f c . . . . 
-. f d d d d e e e f f . . . . . 
-. . f f f f f e e e e f . . . . 
-. . . . f f e e e e e e f . f f 
-. . . f e e f e e f e e f . e f 
-. . f e e f e e f e e e f . e f 
-. f b d f d b f b b f e f f e f 
-. f d d f d d f d d b e f f f f 
-. . f f f f f f f f f f f f f . 
-`;
-            case "image16":return img`
-. . . . c c c b b b b b . . . . 
-. . c c b 4 4 4 4 4 4 b b b . . 
-. c c 4 4 4 4 4 5 4 4 4 4 b c . 
-. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
-e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
-e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
-e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
-. e b 4 4 4 4 4 5 4 4 4 4 b e . 
-8 7 e e b 4 4 4 4 4 4 b e e 6 8 
-8 7 2 e e e e e e e e e e 2 7 8 
-e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
-e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
-e b e 8 8 c c 8 8 c c c 8 e b e 
-e e b e c c e e e e e c e b e e 
-. e e b b 4 4 4 4 4 4 4 4 e e . 
-. . . c c c c c e e e e e . . . 
-`;
-            case "image17":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . 6 6 6 6 6 6 6 6 . . 
-. . . . . 6 c 6 6 6 6 6 6 9 6 . 
-. . . . 6 c c 6 6 6 6 6 6 9 c 6 
-. . d 6 9 c c 6 9 9 9 9 9 9 c c 
-. d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
-. 6 6 6 9 b 8 8 b b b 8 b b 8 6 
-. 6 6 6 6 6 8 b b b b 8 b b b 8 
-. 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
-. 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
-. d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
-. 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
-. 8 8 8 8 f f f 8 8 8 8 f f f f 
-. . . 8 f f f f f 8 8 f f f f f 
-. . . . f f f f . . . . f f f . 
-. . . . . . . . . . . . . . . . 
-`;
-            case "image18":return img`
-eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
-eeebbddddddbbeeeeeebbddddddbbeee
-eebbddbbbbddbbeeeebbddbbbbddbbee
-eedbdbddddbdbdeeeedbdbddddbdbdee
-efdbddbbbbddbdeeefdbddbbbbddbdee
-efbdbbddddbbdbeeefbdbbddddbbdbee
-efedddbbbbdddeeeefedddbbbbdddeee
-effebddddddbeefeeffebddddddbeefe
-effeeeeeeeeeeefeeffeeeeeeeeeeefe
-effeeefeeeeeeefeeffeeefeeeeeeefe
-effefeeeffeefeefeffefeeeffeefeef
-efeefefefefefeeeefeefefefefefeee
-ffeeeefeeffeffeeffeeeefeeffeffee
-feeeeffeeeeefffefeeeeffeeeeefffe
-eeeefffefeeeefffeeeefffefeeeefff
-eeeefffefeeeefffeeeefffefeeeefff
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-`;
-            case "image19":return img`
-eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
-eeebbddddddbbeeeeeebbddddddbbeee
-eebbddbbbbddbbeeeebbddbbbbddbbee
-eedbdbddddbdbdeeeedbdbddddbdbdee
-efdbddbbbbddbdeeefdbddbbbbddbdee
-efbdbbddddbbdbeeefbdbbddddbbdbee
-efedddbbbbdddeeeefedddbbbbdddeee
-effebddddddbeefeeffebddddddbeefe
-effeeeeeeeeeeefeeffeeeeeeeeeeefe
-effeeefeeeeeeefeeffeeefeeeeeeefe
-effefeeeffeefeefeffefeeeffeefeef
-efeefefefefefeeeefeefefefefefeee
-ffeeeefeeffeffeeffeeeefeeffeffee
-feeeeffeeeeefffefeeeeffeeeeefffe
-eeeefffefeeeefffeeeefffefeeeefff
-eeeefffefeeeefffeeeefffefeeeefff
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-`;
-            case "image20":return img`
 eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
 eeebbddddddbbeeeeeebbddddddbbeee
 eebbddbbbbddbbeeeebbddbbbbddbbee
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -5,24 +5,6 @@ namespace myTiles {
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`0a000f00000000000000000000000000000000000000000000000000000000000000010101010101010101010101010101010101010100000000000000000000000000000000000000000202020202020202020202020202020202020202020202020202020202020000000000000000000000000000000000000000010101010101010101010000000000000000000000000000000000000000`, img`
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
-`, [myTiles.transparency16,sprites.dungeon.hazardWater,sprites.vehicle.roadHorizontal], TileScale.Sixteen);
             case "level2":
             case "level2":return tiles.createTilemap(hex`0a000f00000000000000000000000202020202020202020200000000000000000000000000000000000000000101010101010101010101010101010101010101000000000000000000000000000000000000000002020202020202020202000000000000000000000000000000000000000001010101010101010101000000000000000000000000000000000000000000000000000000000000`, img`
 . . . . . . . . . . 
@@ -60,6 +42,24 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a000f000f0e10100f101010100e10100f0f0e10100f10100e10100f101010100e1001010101010101010101010101010101010101010a08080808080808080c0b09090909090909090d0202020202020202020202020202020202020202020202020202020202020304040405050305050305050504030404040405010101010101010101010606060606060606060607070707070707070707`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.hazardWater,sprites.vehicle.roadHorizontal,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.builtin.forestTiles22,sprites.builtin.forestTiles19,sprites.castle.tilePath2,sprites.castle.tilePath8,sprites.castle.tilePath1,sprites.castle.tilePath7,sprites.castle.tilePath3,sprites.castle.tilePath9,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass3], TileScale.Sixteen);
         }
         return null;
     })
