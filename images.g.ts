fcee6c46083bba239c4ef1b1a4061a1882e821b7
erictan88 <69663089+erictan88@users.noreply.github.com>
2021-01-29 18:19:00 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,414 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 6 6 6 6 6 . . 
+. . . . . 6 c 6 6 6 6 6 6 9 6 . 
+. . . . 6 c c 6 6 6 6 6 6 9 c 6 
+. . d 6 9 c c 6 9 9 9 9 9 9 c c 
+. d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+. 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+. 6 6 6 6 6 8 b b b b 8 b b b 8 
+. 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+. 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+. d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+. 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+. 8 8 8 8 f f f 8 8 8 8 f f f f 
+. . . 8 f f f f f 8 8 f f f f f 
+. . . . f f f f . . . . f f f . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image5":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . . . 
+c c c c c d d e e e f c . . . . 
+. f d d d d e e e f f . . . . . 
+. . f f f f f e e e e f . . . . 
+. . . . f f e e e e e e f . f f 
+. . . f e e f e e f e e f . e f 
+. . f e e f e e f e e e f . e f 
+. f b d f d b f b b f e f f e f 
+. f d d f d d f d d b e f f f f 
+. . f f f f f f f f f f f f f . 
+`;
+            case "image6":return img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`;
+            case "image7":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 3 3 3 3 3 3 3 3 . . . . 
+. . . 3 d 3 3 3 3 3 3 c 3 . . . 
+. . 3 c d 3 3 3 3 3 3 c c 3 . . 
+. 3 c c d d d d d d 3 c c d 3 d 
+. 3 c 3 a a a a a a a b c d 3 3 
+. 3 3 a b b a b b b a a b d 3 3 
+. 3 a b b b a b b b b a 3 3 3 3 
+. a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+. a a a a a a f a a a f a 3 d d 
+. a a a a a a f a a f a a a 3 d 
+. a a a a a a f f f a a a a a a 
+. a f f f f a a a a f f f a a a 
+. . f f f f f a a f f f f f a . 
+. . . f f f . . . . f f f f . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image8":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 3 3 3 3 3 3 3 3 . . . . 
+. . . 3 d 3 3 3 3 3 3 c 3 . . . 
+. . 3 c d 3 3 3 3 3 3 c c 3 . . 
+. 3 c c d d d d d d 3 c c d 3 d 
+. 3 c 3 a a a a a a a b c d 3 3 
+. 3 3 a b b a b b b a a b d 3 3 
+. 3 a b b b a b b b b a 3 3 3 3 
+. a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+. a a a a a a f a a a f a 3 d d 
+. a a a a a a f a a f a a a 3 d 
+. a a a a a a f f f a a a a a a 
+. a f f f f a a a a f f f a a a 
+. . f f f f f a a f f f f f a . 
+. . . f f f . . . . f f f f . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeeeeeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeeeeeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbeeeebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdeeeedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdeeefdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbeeefbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeeeefedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefeeffebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefeeffeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefeeffeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeefeffefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeeeefeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffeeffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffefeeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image2":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image9":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 6 6 6 6 6 . . 
+. . . . . 6 c 6 6 6 6 6 6 9 6 . 
+. . . . 6 c c 6 6 6 6 6 6 9 c 6 
+. . d 6 9 c c 6 9 9 9 9 9 9 c c 
+. d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+. 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+. 6 6 6 6 6 8 b b b b 8 b b b 8 
+. 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+. 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+. d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+. 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+. 8 8 8 8 f f f 8 8 8 8 f f f f 
+. . . 8 f f f f f 8 8 f f f f f 
+. . . . f f f f . . . . f f f . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image10":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image11":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image12":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image13":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . . . 
+c c c c c d d e e e f c . . . . 
+. f d d d d e e e f f . . . . . 
+. . f f f f f e e e e f . . . . 
+. . . . f f e e e e e e f . f f 
+. . . f e e f e e f e e f . e f 
+. . f e e f e e f e e e f . e f 
+. f b d f d b f b b f e f f e f 
+. f d d f d d f d d b e f f f f 
+. . f f f f f f f f f f f f f . 
+`;
+            case "image14":return img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`;
+            case "image15":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . . . 
+c c c c c d d e e e f c . . . . 
+. f d d d d e e e f f . . . . . 
+. . f f f f f e e e e f . . . . 
+. . . . f f e e e e e e f . f f 
+. . . f e e f e e f e e f . e f 
+. . f e e f e e f e e e f . e f 
+. f b d f d b f b b f e f f e f 
+. f d d f d d f d d b e f f f f 
+. . f f f f f f f f f f f f f . 
+`;
+            case "image16":return img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`;
+            case "image17":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 6 6 6 6 6 . . 
+. . . . . 6 c 6 6 6 6 6 6 9 6 . 
+. . . . 6 c c 6 6 6 6 6 6 9 c 6 
+. . d 6 9 c c 6 9 9 9 9 9 9 c c 
+. d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+. 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+. 6 6 6 6 6 8 b b b b 8 b b b 8 
+. 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+. 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+. d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+. 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+. 8 8 8 8 f f f 8 8 8 8 f f f f 
+. . . 8 f f f f f 8 8 f f f f f 
+. . . . f f f f . . . . f f f . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image18":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image19":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+            case "image20":return img`
+eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+eeebbddddddbbeeeeeebbddddddbbeee
+eebbddbbbbddbbeeeebbddbbbbddbbee
+eedbdbddddbdbdeeeedbdbddddbdbdee
+efdbddbbbbddbdeeefdbddbbbbddbdee
+efbdbbddddbbdbeeefbdbbddddbbdbee
+efedddbbbbdddeeeefedddbbbbdddeee
+effebddddddbeefeeffebddddddbeefe
+effeeeeeeeeeeefeeffeeeeeeeeeeefe
+effeeefeeeeeeefeeffeeefeeeeeeefe
+effefeeeffeefeefeffefeeeffeefeef
+efeefefefefefeeeefeefefefefefeee
+ffeeeefeeffeffeeffeeeefeeffeffee
+feeeeffeeeeefffefeeeeffeeeeefffe
+eeeefffefeeeefffeeeefffefeeeefff
+eeeefffefeeeefffeeeefffefeeeefff
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,41 +1,47 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Hero.y += -16
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Hero.x += -16
+})
 scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
     sprite.destroy()
 })
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Hero.x += 16
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardWater, function (sprite, location) {
-    if (Hero.overlapsWith(Raft) || (Hero.overlapsWith(Raft2) || Hero.overlapsWith(Raft3))) {
+    if (Hero.overlapsWith(Raft) || Hero.overlapsWith(Raft3)) {
         Hero.setVelocity(50, 0)
+    } else if (Hero.overlapsWith(Raft2)) {
+        Hero.setVelocity(-40, 0)
     } else {
-        game.over(false)
+        info.changeLifeBy(-1)
+        sprite.y += 16
     }
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Hero.y += 16
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    tiles.placeOnTile(Hero, tiles.getTileLocation(5, 14))
 })
 scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     sprite.destroy()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.over(false)
+    info.changeLifeBy(-1)
+    sprite.y += 16
 })
+let Car02: Sprite = null
+let Car03: Sprite = null
 let Car01: Sprite = null
-let Raft3: Sprite = null
 let Raft2: Sprite = null
+let Raft3: Sprite = null
 let Raft: Sprite = null
 let Hero: Sprite = null
-tiles.setTilemap(tiles.createTilemap(hex`0a000f00000000000000000000000202020202020202020200000000000000000000000000000000000000000101010101010101010101010101010101010101000000000000000000000000000000000000000002020202020202020202000000000000000000000000000000000000000001010101010101010101000000000000000000000000000000000000000000000000000000000000`, img`
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    . . . . . . . . . . 
-    `, [myTiles.transparency16,sprites.dungeon.hazardWater,sprites.vehicle.roadHorizontal], TileScale.Sixteen))
+tiles.setTilemap(tilemap`level1`)
 Hero = sprites.create(img`
     . . . . f f f f f . . . . . . . 
     . . . f e e e e e f . . . . . . 
@@ -75,8 +81,9 @@ let End = sprites.create(img`
 tiles.placeOnTile(Hero, tiles.getTileLocation(5, 14))
 tiles.placeOnTile(End, tiles.getTileLocation(5, 0))
 scene.cameraFollowSprite(Hero)
-controller.moveSprite(Hero)
-game.onUpdateInterval(2000, function () {
+info.setLife(3)
+Hero.z = 100
+game.onUpdateInterval(2500, function () {
     Car01 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . 6 6 6 6 6 6 6 6 . . 
@@ -96,7 +103,7 @@ game.onUpdateInterval(2000, function () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     tiles.placeOnTile(Car01, tiles.getTileLocation(9, 8))
-    Car01.setVelocity(randint(-10, -60), 0)
+    Car01.setVelocity(randint(-30, -50), 0)
 })
 game.onUpdateInterval(2000, function () {
     Raft = sprites.create(img`
@@ -121,34 +128,56 @@ game.onUpdateInterval(2000, function () {
         eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
         eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
         `, SpriteKind.Projectile)
-    tiles.placeOnTile(Raft, tiles.getTileLocation(0, 11))
+    tiles.placeOnTile(Raft, tiles.getTileLocation(0, 12))
     Raft.setVelocity(50, 0)
 })
 game.onUpdateInterval(2000, function () {
+    Car03 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 3 3 3 3 3 3 3 3 . . . . 
+        . . . 3 d 3 3 3 3 3 3 c 3 . . . 
+        . . 3 c d 3 3 3 3 3 3 c c 3 . . 
+        . 3 c c d d d d d d 3 c c d 3 d 
+        . 3 c 3 a a a a a a a b c d 3 3 
+        . 3 3 a b b a b b b a a b d 3 3 
+        . 3 a b b b a b b b b a 3 3 3 3 
+        . a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+        . a a a a a a f a a a f a 3 d d 
+        . a a a a a a f a a f a a a 3 d 
+        . a a a a a a f f f a a a a a a 
+        . a f f f f a a a a f f f a a a 
+        . . f f f f f a a f f f f f a . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnTile(Car03, tiles.getTileLocation(0, 7))
+    Car03.setVelocity(randint(40, 70), 0)
+})
+game.onUpdateInterval(2000, function () {
     Raft2 = sprites.create(img`
-        eeeeebbbbbbeeeeeeeeeebbbbbbeeeee
-        eeebbddddddbbeeeeeebbddddddbbeee
-        eebbddbbbbddbbeeeebbddbbbbddbbee
-        eedbdbddddbdbdeeeedbdbddddbdbdee
-        efdbddbbbbddbdeeefdbddbbbbddbdee
-        efbdbbddddbbdbeeefbdbbddddbbdbee
-        efedddbbbbdddeeeefedddbbbbdddeee
-        effebddddddbeefeeffebddddddbeefe
-        effeeeeeeeeeeefeeffeeeeeeeeeeefe
-        effeeefeeeeeeefeeffeeefeeeeeeefe
-        effefeeeffeefeefeffefeeeffeefeef
-        efeefefefefefeeeefeefefefefefeee
-        ffeeeefeeffeffeeffeeeefeeffeffee
-        feeeeffeeeeefffefeeeeffeeeeefffe
-        eeeefffefeeeefffeeeefffefeeeefff
-        eeeefffefeeeefffeeeefffefeeeefff
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeebbbbbbeeeeeeeeeebbbbbbeeeeeeeeeebbbbbbeeeeeeeeeebbbbbbeeeee
+        eeebbddddddbbeeeeeebbddddddbbeeeeeebbddddddbbeeeeeebbddddddbbeee
+        eebbddbbbbddbbeeeebbddbbbbddbbeeeebbddbbbbddbbeeeebbddbbbbddbbee
+        eedbdbddddbdbdeeeedbdbddddbdbdeeeedbdbddddbdbdeeeedbdbddddbdbdee
+        efdbddbbbbddbdeeefdbddbbbbddbdeeefdbddbbbbddbdeeefdbddbbbbddbdee
+        efbdbbddddbbdbeeefbdbbddddbbdbeeefbdbbddddbbdbeeefbdbbddddbbdbee
+        efedddbbbbdddeeeefedddbbbbdddeeeefedddbbbbdddeeeefedddbbbbdddeee
+        effebddddddbeefeeffebddddddbeefeeffebddddddbeefeeffebddddddbeefe
+        effeeeeeeeeeeefeeffeeeeeeeeeeefeeffeeeeeeeeeeefeeffeeeeeeeeeeefe
+        effeeefeeeeeeefeeffeeefeeeeeeefeeffeeefeeeeeeefeeffeeefeeeeeeefe
+        effefeeeffeefeefeffefeeeffeefeefeffefeeeffeefeefeffefeeeffeefeef
+        efeefefefefefeeeefeefefefefefeeeefeefefefefefeeeefeefefefefefeee
+        ffeeeefeeffeffeeffeeeefeeffeffeeffeeeefeeffeffeeffeeeefeeffeffee
+        feeeeffeeeeefffefeeeeffeeeeefffefeeeeffeeeeefffefeeeeffeeeeefffe
+        eeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefff
+        eeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefffeeeefffefeeeefff
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
         `, SpriteKind.Projectile)
-    tiles.placeOnTile(Raft2, tiles.getTileLocation(9, 5))
-    Raft2.setVelocity(-50, 0)
+    tiles.placeOnTile(Raft2, tiles.getTileLocation(9, 3))
+    Raft2.setVelocity(-40, 0)
 })
 game.onUpdateInterval(2000, function () {
     Raft3 = sprites.create(img`
@@ -175,4 +204,29 @@ game.onUpdateInterval(2000, function () {
         `, SpriteKind.Projectile)
     tiles.placeOnTile(Raft3, tiles.getTileLocation(0, 4))
     Raft3.setVelocity(50, 0)
+})
+game.onUpdateInterval(100, function () {
+    Hero.setVelocity(0, 0)
+})
+game.onUpdateInterval(3000, function () {
+    Car02 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 3 3 3 3 3 3 3 3 . . . . 
+        . . . 3 d 3 3 3 3 3 3 c 3 . . . 
+        . . 3 c d 3 3 3 3 3 3 c c 3 . . 
+        . 3 c c d d d d d d 3 c c d 3 d 
+        . 3 c 3 a a a a a a a b c d 3 3 
+        . 3 3 a b b a b b b a a b d 3 3 
+        . 3 a b b b a b b b b a 3 3 3 3 
+        . a a 3 3 3 a 3 3 3 3 3 a 3 3 3 
+        . a a a a a a f a a a f a 3 d d 
+        . a a a a a a f a a f a a a 3 d 
+        . a a a a a a f f f a a a a a a 
+        . a f f f f a a a a f f f a a a 
+        . . f f f f f a a f f f f f a . 
+        . . . f f f . . . . f f f f . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnTile(Car02, tiles.getTileLocation(0, 9))
+    Car02.setVelocity(randint(20, 30), 0)
 })
